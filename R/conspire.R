5ac646fd153c7d4d7f56e7feee1b0e5291c107bd
Lingtax <m.ling@deakin.edu.au>
2021-03-02 11:13:43 +1100

    extend dictionaries

--- R/conspire.R
+++ R/conspire.R
@@ -8,12 +8,23 @@
 #' conspirator::conspire()
 conspire <- function() {
   
-  agent <-  list("The government is", "Aliens are", "Snake people are", "Very angry quolls are", 
-                 "Underappreciated household appliances are")
-  adjective <-  list("secretly", "covertly", "surreptitiously")
+  agent <-  list("The government is", 
+                 "Aliens are", 
+                 "Snake people are", 
+                 "Very angry quolls are", 
+                 "Underappreciated household appliances are",
+                 "Big Pharma is", 
+                 "Scientists are")
+  
+  adjective <-  list("secretly", 
+                     "covertly", 
+                     "surreptitiously")
+  
   action <- list("swapping toothpaste for moisturiser.",
                  "putting up roadwork signs where there is no planned roadwork.",
-                 "planning to unify against humanity.")
+                 "planning to unify against humanity.",
+                 "rearranging your cutlery drawer while you sleep.", 
+                 "stealing one sock per load of laundry.")
   
   paste(sample(agent, 1), 
         sample(adjective, 1),
