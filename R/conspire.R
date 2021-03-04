70dea1f433f946f38279c63384bdb6096a3acf85
Lingtax <m.ling@deakin.edu.au>
2021-03-04 16:11:23 +1100

    even more conspiracies

--- R/conspire.R
+++ R/conspire.R
@@ -17,7 +17,12 @@ conspire <- function() {
                  "Scientists are", 
                  "Sulphur-crested cockatoos are", 
                  "Cats are", 
-                 "Corporations are"
+                 "Corporations are", 
+                 "Drug barons are",
+                 "The population of peoples named Mathew, Matthew, Matvei, Matej, Matko, Matityahu, Mateus, Matteo, etc. are",
+                 "The Knights Templar are", 
+                 "The less-widely-known Knights Hospitalier are", 
+                 "A ritual-soaked conclave of turners and fitters are"
                  )
   
   adjective <-  list("secretly", 
@@ -36,7 +41,11 @@ conspire <- function() {
                  "faking moon landings.",
                  "putting microchips that track your location and activities into your mobile phone.",
                  "engaging in insider trading.",
-                 "putting chemicals like sodium, chlorine, and even occasionally iodine in salt."
+                 "putting chemicals like sodium, chlorine, and even occasionally iodine in salt.",
+                 "raising an underground army of mole-people.", 
+                 "disseminating rumours about fake conspiracies.", 
+                 "placing mysterious monoliths and obelisks around the world.", 
+                 "green-lighting terrible Netflix originals."
                  )
   
   paste(sample(agent, 1), 
